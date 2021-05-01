8d8af55c4987dbf199524afbed85c67383fcf366
aengun <aengun@icloud.com>
2021-05-02 00:17:05 +0900

    ~19장 React.memo까지

--- src/App.js
+++ src/App.js
@@ -1,39 +1,102 @@
-import React, { useRef } from "react";
-import UserList from "./UserList";
-import { useRef } from "react";
+import React, { useRef, useState, useMemo, useCallback } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs],
+  );
+
+  const [users, setUsers] = useState([
     {
       id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: false,
     },
     {
       id: 2,
-      username: "tester",
-      email: "tester@gmail.com",
+      username: 'tester',
+      email: 'tester@gmail.com',
+      active: false,
     },
     {
       id: 3,
-      username: "liz",
-      email: "liz@gmail.com",
+      username: 'liz',
+      email: 'liz@gmail.com',
+      active: false,
     },
-  ];
+  ]);
 
   //useRef는 특정 돔을 선택할 때 쓸 수도 있지만
   //어떠한 변수를 기억하고 싶을 때 사용 가능
   //이 값은 컴포넌트가 리랜더링되어도 사용할 수 있다.
   const nextId = useRef(4);
 
-  const onCreate = () => {
-    console.log(nextId.current); //4
-
+  const onCreate = useCallback(() => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers((users) => users.concat(user));
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
-    
-  };
+  }, [username, email]);
 
-  return <UserList users={users} />;
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user,
+      ),
+    );
+  }, []);
+
+  // const count = countActiveUsers(users);
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수: {count}</div>
+    </>
+  );
 }
 
 export default App;
+
+// 연산된 값 재사용 : useMemo
+// 특정함수 재사용 : useCallback
+// 컴포넌트 렌더링된 결과물 재사용 : React.memo
--- src/UserList.js
+++ src/UserList.js
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-function User({ user }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
+  // useEffect(() => {
+  //   // props => state
+  //   // REST API
+  //   // 라이브러리 사용 D3 video.js
+  //   // setInterval, setTimeout
+  //   // 위를 사용할 때
+  //   console.log('컴포넌트가 화면에 나타남');
+  //   return () => {
+  //     // clearInterval, clearTimeout
+  //     // 라이브러리 인스턴스 제거
+  //     console.log('컴포넌트가 화면에서 사라짐');
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log('user 값이 설정됨');
+  //   console.log(user);
+  //   return () => {
+  //     console.log('user 값이 바뀌기 전');
+  //     console.log(user);
+  //   };
+  // }, [user]);
+
   return (
     <div>
-      <b>
-        {user.username} <span>({user.email})</span>
+      <b
+        style={{
+          color: active ? 'green' : 'black',
+          cursor: 'pointer',
+        }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
       </b>
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
-}
+});
 
-function UserList({users}) {
-  
-
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id}/>
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
 }
 
-export default UserList;
+export default React.memo(
+  UserList,
+  (prevProps, nextProps) => nextProps.users === prevProps.users,
+);
